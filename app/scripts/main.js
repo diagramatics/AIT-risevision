efd6079bd5a09fbf81dd1439ee2b7541db8dde21
Steven Sinatra <ohai@diagramatics.me>
2015-04-13 04:35:58 +1000

    Mockup of the student gallery system

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,2 +1,123 @@
 /* jshint devel:true */
-console.log('\'Allo \'Allo!');
+'use strict';
+
+class Gallery {
+  constructor() {
+    this.slideDuration = 10000;
+    this.images = [
+      {
+        'image': '/images/lorem-image.jpg',
+        'name': 'Lorem Graphics',
+        'author': 'Ipsum',
+        'text': '<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deleniti quo beatae non voluptate, modi facere culpa exercitationem? Cum, laborum, deleniti. Quae sapiente omnis, repellendus.</p><p>Nesciunt molestiae possimus ea quidem! Cumque delectus provident, itaque at exercitationem quis tenetur iusto minima est atque! Magnam suscipit perspiciatis laboriosam, molestiae sapiente consequuntur!</p><p>Placeat quae doloribus neque cupiditate consequatur reiciendis voluptates deleniti esse, ipsam maiores provident aspernatur blanditiis, culpa quas! Tenetur, recusandae pariatur temporibus minus cupiditate, optio.</p>'
+      },
+      {
+        'image': '/images/lorem-image-2.jpg',
+        'name': 'Lorem Art',
+        'author': 'Lorema',
+        'text': '<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deleniti quo beatae non voluptate, modi facere culpa exercitationem? Cum, laborum, deleniti. Quae sapiente omnis, repellendus.</p><p>Placeat quae doloribus neque cupiditate consequatur reiciendis voluptates deleniti esse, ipsam maiores provident aspernatur blanditiis, culpa quas! Tenetur, recusandae pariatur temporibus minus cupiditate, optio.</p><p>Nesciunt molestiae possimus ea quidem! Cumque delectus provident, itaque at exercitationem quis tenetur iusto minima est atque! Magnam suscipit perspiciatis laboriosam, molestiae sapiente consequuntur!</p>'
+      }
+    ];
+
+    this.el = {
+      'root': $('#gallery'),
+      'image': $('#galleryImage'),
+      'background': $('#galleryBackground'),
+      'captionBackground': $('#galleryCaptionBackground'),
+      'caption': $('#galleryCaption'),
+      'miniInfo': $('#galleryMiniInfo')
+    }
+
+    // Set the animation length to be the same as the slide duration
+    this.el.background.css('animation-duration', (this.slideDuration * 4/3) + 'ms');
+    this.el.captionBackground.css('animation-duration', (this.slideDuration * 4/3) + 'ms');
+
+    this.imageIterator = Math.floor(Math.random() * this.images.length);
+
+    var dataImage = $('#gallery').attr('data-src');
+    this.loadImage(this.images[this.imageIterator++]);
+    this.cycleImages();
+
+    var duplicates = false, i = 0;
+    for (var i = 0; i < this.images.length; i++) {
+      if (this.images[i].image === dataImage) {
+        duplicates = true;
+        break;
+      }
+    }
+    if (!duplicates) {
+      this.image.push(dataImage);
+    }
+  }
+
+  cycleImages() {
+    let self = this;
+    setInterval(function() {
+      if (++self.imageIterator > self.images.length - 1) {
+        // Reset back to the first array if cycled past the last one
+        self.imageIterator = 0;
+      }
+      // Set the image and the caption
+      self.loadImage(self.images[self.imageIterator]);
+
+      // Set a timer to show the caption
+      setTimeout(function() {
+        self.el.caption.addClass('display');
+        self.el.captionBackground.addClass('display');
+        self.el.miniInfo.addClass('no-display');
+      }, self.slideDuration / 3)
+
+      // Another one to hide them at the last 4 seconds
+      setTimeout(function() {
+        self.el.caption.removeClass('display');
+        self.el.captionBackground.removeClass('display');
+        self.el.miniInfo.removeClass('no-display');
+      }, self.slideDuration - 5000)
+
+      // Toggle the loading screen on the last two seconds
+      setTimeout(function() {
+        self.el.root.addClass('loading');
+      }, self.slideDuration - 2000)
+    }, this.slideDuration);
+  }
+
+  formatCaption(image) {
+    return '<h1>'+ image.name +'</h1><p>By '+ image.author +'</p>' + image.text;
+  }
+
+  formatMiniInfo(image) {
+    return '<strong>'+ image.name +'</strong> by <em>'+ image.author +'</em>';
+  }
+
+  loadImage(image) {
+    let self = this;
+    let imageFormatted = 'url('+ image.image +')';
+    // Disable animation and enable loading screen
+    this.el.background.removeClass('animate');
+    this.el.captionBackground.removeClass('animate');
+
+
+    this.el.image.attr('src', image.image);
+    this.el.background.css('background-image', imageFormatted);
+    this.el.captionBackground.css('background-image', imageFormatted);
+    this.el.caption.html(this.formatCaption(image));
+    this.el.miniInfo.html(this.formatMiniInfo(image));
+
+    // Remove loading screen and enable animation again
+    setTimeout(function() {
+      self.el.background.addClass('animate');
+      self.el.captionBackground.addClass('animate');
+      self.el.root.removeClass('loading').addClass('loaded');
+    }, 500);
+
+    // Last animations for loading screen
+    setTimeout(function() {
+      self.el.root.removeClass('loaded');
+    }, this.slideDuration / 2)
+  }
+
+}
+
+$(function() {
+  var gallery = new Gallery();
+});
--- gulpfile.js
+++ gulpfile.js
@@ -23,6 +23,15 @@ gulp.task('styles', function () {
     .pipe(reload({stream: true}));
 });
 
+gulp.task('scripts', function() {
+  return gulp.src('app/scripts/**/*.js')
+    .pipe($.sourcemaps.init())
+    .pipe($.babel())
+    .pipe($.sourcemaps.write())
+    .pipe(gulp.dest('.tmp/scripts'))
+    .pipe(reload({stream: true}));
+});
+
 gulp.task('jshint', function () {
   return gulp.src('app/scripts/**/*.js')
     .pipe(reload({stream: true, once: true}))
@@ -31,7 +40,7 @@ gulp.task('jshint', function () {
     .pipe($.if(!browserSync.active, $.jshint.reporter('fail')));
 });
 
-gulp.task('html', ['styles'], function () {
+gulp.task('html', ['styles', 'scripts'], function () {
   var assets = $.useref.assets({searchPath: ['.tmp', 'app', '.']});
 
   return gulp.src('app/*.html')
@@ -75,7 +84,7 @@ gulp.task('extras', function () {
 
 gulp.task('clean', require('del').bind(null, ['.tmp', 'dist']));
 
-gulp.task('serve', ['styles', 'fonts'], function () {
+gulp.task('serve', ['styles', 'scripts', 'fonts'], function () {
   browserSync({
     notify: false,
     port: 9000,
@@ -90,12 +99,12 @@ gulp.task('serve', ['styles', 'fonts'], function () {
   // watch for changes
   gulp.watch([
     'app/*.html',
-    'app/scripts/**/*.js',
     'app/images/**/*',
     '.tmp/fonts/**/*'
   ]).on('change', reload);
 
   gulp.watch('app/styles/**/*.scss', ['styles']);
+  gulp.watch('app/scripts/**/*.js', ['scripts']);
   gulp.watch('app/fonts/**/*', ['fonts']);
   gulp.watch('bower.json', ['wiredep', 'fonts']);
 });
